9561d3f1d93837cbcd7c456407b3943926d1a77f
Your Name <pragaa3633533@gmail.com>
2024-07-29 07:31:13 +0530

    m

--- services/twillio.js
+++ services/twillio.js
@@ -1,8 +1,8 @@
 const twilio = require('twilio');
 
 // Replace these values with your Twilio Account SID and Auth Token
-const accountSid ='AC83b7c3ebfef86c7035a259352a6c5342';
-const authToken ='76c302b8302b9942dc2f97a8437d9e6c';
+const accountSid ='';
+const authToken ='';
 
 // Initialize Twilio client
 const client = twilio(accountSid, authToken);
